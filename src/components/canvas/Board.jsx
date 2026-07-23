8c7f79548585c42bde16d7a4912c8b64c20194b3
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-24 03:45:39 +0700

    Update Board.jsx

--- src/components/canvas/Board.jsx
+++ src/components/canvas/Board.jsx
@@ -345,7 +345,7 @@ export default function Board() {
     <div
       ref={wrapRef}
       className="fixed inset-0 cursor-grab touch-none select-none overflow-hidden"
-      style={{ backgroundColor: '#000000', zIndex: 999, border: '6px solid red', position: 'fixed', inset: 0 }}
+      style={{ backgroundColor: '#000000', zIndex: 50, border: '6px solid red', position: 'fixed', inset: 0 }}
     >
       <div
         ref={innerRef}
