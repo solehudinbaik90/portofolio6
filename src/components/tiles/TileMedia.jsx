3620f0fea37c966c9c84b6587e62f1b9fdfe6a49
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-25 14:52:16 +0700

    Update TileMedia.jsx

--- src/components/tiles/TileMedia.jsx
+++ src/components/tiles/TileMedia.jsx
@@ -2,7 +2,8 @@ import { forwardRef } from 'react';
 import { useHoverDevice } from '../../hooks/useHoverDevice';
 import VideoBadge from './VideoBadge';
 
-const BLUR_CLASSES = 'pointer-events-none size-full rounded-[inherit] object-cover blur-[32px] scale-125 group-hover:blur-[0px] group-hover:scale-100 group-[.discovered]:blur-[0px] group-[.discovered]:scale-100';
+const BLUR_CLASSES = 'pointer-events-none size-full rounded-[inherit] object-cover transition-all duration-500 ease-out group-hover:scale-105';
+
 
 const TileMedia = forwardRef(function TileMedia({ tile, isFocused }, ref) {
   const isHover = useHoverDevice();
