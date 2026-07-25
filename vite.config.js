e527e0b8b81f99239a8a520c993cbb54b242639b
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-25 14:08:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  },
 });
