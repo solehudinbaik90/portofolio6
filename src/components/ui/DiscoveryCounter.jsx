b5130ce67069058a95fde47f3db3b65b771bf4c2
Muhamad Soleh <solehudinz3692@gmail.com>
2026-07-27 19:38:52 +0700

    Update DiscoveryCounter.jsx

--- src/components/ui/DiscoveryCounter.jsx
+++ src/components/ui/DiscoveryCounter.jsx
@@ -133,7 +133,7 @@ export default function DiscoveryCounter() {
 
 
   return (
-    <div className="pointer-events-none fixed bottom-8 right-8 z-20">
+    <div className="pointer-events-none fixed bottom-8 right-8 z-20 hidden min-[768px]:block">
 
       <div className="pointer-events-none absolute bottom-full right-0 mb-4">
         <div
